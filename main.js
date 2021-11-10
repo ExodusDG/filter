f678abbd563776fd851c0eb869109a9826c8b57d
Mykola Pererbenesiuk <sanistray@gmail.com>
2021-11-10 18:49:33 +0200

    1

--- main.js
+++ main.js
@@ -15,21 +15,30 @@ $(function() {
         range: true,
         min: 150,
         max: 1500,
-        values: [400, 1000],
+        values: [150, 400],
         slide: function(event, ui) {
-            $("#amount2").val(ui.values[0] + " kms" + " - " + ui.values[1] + " kms");
+            $("#amount2").val("150 kms" + " - " + ui.values[1] + " kms");
 
             if (ui.values[1] == $("#slider-range_2").slider("option", "max")) {
-
                 $("#amount2").val("Entire Country")
             }
         }
     });
     $("#amount2").val($("#slider-range_2").slider("values", 0) + " kms" +
         " - " + $("#slider-range_2").slider("values", 1) + " kms");
+    $('.ui-slider-handle:eq(2)').hide()
 });
+
+
+
+
+
 
 $('.filter__gender_choise > p').click(function() {
     $('.filter__gender_choise > p').removeClass('gradient-text')
     $(this).addClass('gradient-text')
+})
+
+$('.filter__header_close').click(function() {
+    $('.filter').attr('style', 'display: none;')
 })
